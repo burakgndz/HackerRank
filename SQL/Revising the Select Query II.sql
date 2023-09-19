b1abcc1496b12e73ccb105e92e53902bcba0dd37
burakgndz <56515947+burakgndz@users.noreply.github.com>
2023-09-19 16:21:08 +0300

    Add files via upload

---
+++ SQL/Revising the Select Query II.sql
@@ -0,0 +1,11 @@
+/*
+HackerRank SQL Problem
+Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+*/
+SELECT NAME FROM CITY WHERE COUNTRYCODE='USA' AND POPULATION>120000;
+/* Expected Output
+Scottsdale
+Corona
+Concord
+Cedar Rapids
+*/
