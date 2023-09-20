a2a5fd53a1d66d8907b7b0daaa0f9596bafb9e83
burakgndz <56515947+burakgndz@users.noreply.github.com>
2023-09-20 14:46:50 +0300

    Population Density Difference

---
+++ SQL/Population Density Difference.sql
@@ -0,0 +1,10 @@
+/*
+HackerRank SQL Problem
+Query the difference between the maximum and minimum populations in CITY.
+
+
+*/
+
+SELECT MAX(POPULATION)-MIN(POPULATION) FROM CITY;
+/* Expected Output
+4695354*/
