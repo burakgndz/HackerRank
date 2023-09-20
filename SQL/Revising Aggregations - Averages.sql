745ebb439ad4b1930c2a4515c6ef59542792fb46
burakgndz <56515947+burakgndz@users.noreply.github.com>
2023-09-20 14:40:01 +0300

    Revising Aggregations - Averages

---
+++ SQL/Revising Aggregations - Averages.sql
@@ -0,0 +1,9 @@
+/*
+HackerRank SQL Problem
+Query the average population of all cities in CITY where District is California.
+*/
+
+SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT ='California';
+/* Expected Output
+113000.667
+*/
