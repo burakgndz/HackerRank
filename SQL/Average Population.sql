87a6f08d2c5e41287b806b99c0cc5c2a36c2f774
burakgndz <56515947+burakgndz@users.noreply.github.com>
2023-09-20 14:42:45 +0300

    Average Population

---
+++ SQL/Average Population.sql
@@ -0,0 +1,9 @@
+/*
+HackerRank SQL Problem
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+*/
+
+SELECT ROUND(AVG(POPULATION),0) FROM CITY
+/* Expected Output
+454250
+*/
